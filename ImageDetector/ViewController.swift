eddaba4fe30c88f2bfbcafee816142dea00d7af8
Tom Smallwood <tom@agilerocket.com>
2017-07-12 02:05:43 -0700

    captured video output using AVCaptureVideoDataOutputSampleBuffer, did model setup using VNCoreMLRequest, taking the top 3 observations and outputting them to the identification label

--- ImageDetector/ViewController.swift
+++ ImageDetector/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
 
     let session = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var visionRequests = [VNRequest]()
     
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var identifierLabel: UILabel!
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVideoSession()
+        modelSetup()
     }
     
     func setupVideoSession() {
@@ -45,16 +48,60 @@ class ViewController: UIViewController {
         connection?.videoOrientation = .portrait
         
         session.startRunning()
+        
+        modelSetup()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         previewLayer.frame = self.videoView.bounds;
+    }
+    
+    func modelSetup() {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            fatalError("Can't load Resnet50 ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            // only want the top 3 observations
+            let topObservations = results[0...3].flatMap({ $0.confidence > 0.5 ? $0: nil }).map({ "\($0.identifier) \(String(format:"%.2f", $0.confidence))" })
+                .joined(separator: "\n")
+            
+            DispatchQueue.main.async {
+                self?.identifierLabel.text = topObservations
+            }
+        }
+        request.imageCropAndScaleOption = .centerCrop
+        visionRequests = [request]
     }
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        connection.videoOrientation = .portrait
+        
+        var requestOptions:[VNImageOption: Any] = [:]
+        
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics: cameraIntrinsicData]
+        }
+        
+        // for orientation see kCGImagePropertyOrientation
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 1)!, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+    }
     
 }
 
