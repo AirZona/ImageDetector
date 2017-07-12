19b66068e757e2bb12439293f16d3b51ff5d0ef6
Tom Smallwood <tom@agilerocket.com>
2017-07-12 01:10:05 -0700

    created video stream via AVCaptureSession

--- ImageDetector/ViewController.swift
+++ ImageDetector/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    @IBOutlet weak var videoView: UIView!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupVideoSession()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupVideoSession() {
+        guard let video = AVCaptureDevice.default(for: .video), let videoInput = try? AVCaptureDeviceInput(device: video) else {
+            fatalError("No video camera available")
+        }
+        
+        session.addInput(videoInput)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        videoView.layer.addSublayer(previewLayer)
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        let queue = DispatchQueue(label: "videoQueue")
+        videoOutput.setSampleBufferDelegate(self, queue: queue)
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+        session.sessionPreset = .high
+        session.addOutput(videoOutput)
+        
+        // force portrait only
+        let connection = videoOutput.connection(with: .video)
+        connection?.videoOrientation = .portrait
+        
+        session.startRunning()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = self.videoView.bounds;
+    }
+    
+}
 
-
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
 }
 
