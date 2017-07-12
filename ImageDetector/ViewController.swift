7a9d6978de61c8f1e733beeffbd0d33d8f9363ac
Tom Smallwood <tom@agilerocket.com>
2017-07-12 01:02:33 -0700

    initial commit

---
+++ ImageDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDetector
+//
+//  Created by Thomas Smallwood on 7/12/17.
+//  Copyright © 2017 Agile Rocket Software PLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
